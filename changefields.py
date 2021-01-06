bf235d75055066583ed3c85613d4c2cd23206504
szbarabas1 <77062645+szbarabas1@users.noreply.github.com>
2021-01-06 18:08:38 +0100

    Update changefields.py

--- changefields.py
+++ changefields.py
@@ -6,16 +6,12 @@ indexdate=datetime.today().strftime('%Y.%m.%d')
 print (indexdate)
 
 
-
 '''
 curl -vv -H 'Content-Type: application/json' -XPUT 'localhost:9200/sts_multi_metrics-2020.02.12/_settings' -d ' { "index.mapping.total_fields.limit": 5000 }' 
 '''
 
-
-command="curl -vv -H 'Content-Type: application/json' -XPUT 'stackstate.apps.ocp.qa.ondit.net:9200/sts_multi_metrics-"+indexdate+"/_settings' -d ' { "+'"'+"index.mapping.total_fields.limit"+'"
-'+": 5000 }'"
+command="curl -vv -H 'Content-Type: application/json' -XPUT 'stackstate.apps.ocp.qa.ondit.net:9200/sts_multi_metrics-"+indexdate+"/_settings' -d ' { "+'"'+"index.mapping.total_fields.limit"+'"'+": 5000 }'"
 print (command)
-
 
 #subprocess.check_output(command)
 myprocess=subprocess.run(command,shell=True,timeout=120)
